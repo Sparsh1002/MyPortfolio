4e4ffec96f3156e625f26143b1ee207688c03650
Sparsh Lodha <57362905+Sparsh1002@users.noreply.github.com>
2026-05-29 13:01:32 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,12 +1,18 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Developer - 1',
+    title: 'Software Developer - 2',
     company: "Deloitte India",
-    duration: "(June 2023 - Present)"
+    duration: "(May 2026 - Present)"
   },
   {
     id: 2,
+    title: 'Software Developer - 1',
+    company: "Deloitte India",
+    duration: "(June 2023 - May 2026)"
+  },
+  {
+    id: 3,
     title: "Self Employed",
     company: "Coding and build something in everyday.",
     duration: "(Aug 2019 - Present)"
